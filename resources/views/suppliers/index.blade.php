d2b2813439295c17e86d636623a2097242ef6507
Terry Ferreira <terry@pixelcandy.co.za>
2016-03-31 09:42:49 +0200

    Missed the suppliers index page with dataTables
    Signed-off-by: Terry Ferreira <terry@pixelcandy.co.za>

--- resources/views/suppliers/index.blade.php
+++ resources/views/suppliers/index.blade.php
@@ -9,23 +9,42 @@
         </div>
         <div class="box-body">
           <p><a href="suppliers/create"><button type="button" class="btn btn-default" name="create-new-supplier" data-toggle="tooltip" data-original-title="Create New Supplier"><span class='glyphicon glyphicon-plus' aria-hidden='true'></span> <b>Create New Supplier</b></button></a></p>
-          <table class="table table-striped table-bordered table-hover">
-            <tr>
-              <th>Supplier</th>
-              <th>Actions</th>
-            </tr>
-            @foreach($suppliers as $supplier)
+          <table id="table" class="table table-striped table-bordered table-hover">
+            <thead>
               <tr>
-                <div>
-                  <td>{{$supplier->name}}</td>
-                  <td><a href="/suppliers/{{ $supplier->id }}/edit">Edit</a></td>
-                </div>
+                <th>Supplier</th>
+                <th>Actions</th>
               </tr>
-            @endforeach
+            </thead>
+            <tbody>
+              @foreach($suppliers as $supplier)
+                <tr>
+                  <div>
+                    <td>{{$supplier->name}}</td>
+                    <td><a href="/suppliers/{{ $supplier->id }}/edit">Edit</a></td>
+                  </div>
+                </tr>
+              @endforeach
+            </tbody>
           </table>
         </div>
       </div>
-      <div class="text-center">{!! $suppliers->links() !!}</div>
     </div>
   </div>
+  <script>
+    $(document).ready(function() {
+      $('#table').DataTable( {
+          columnDefs: [ {
+              targets: [ 0 ],
+              orderData: [ 0, 1 ]
+          }, {
+              targets: [ 1 ],
+              orderData: [ 1, 0 ]
+          }, {
+              targets: [ 1 ],
+              orderData: [ 1, 0 ]
+          } ]
+      } );
+    } );
+  </script>
 @endsection
