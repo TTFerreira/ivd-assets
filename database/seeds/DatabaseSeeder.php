5a3d0e8e275805045f87f9855b38ceb618e4eeb4
Terry Ferreira <terry@pixelcandy.co.za>
2016-05-05 09:49:36 +0200

    Altered the DB Seeds for Production primarily
    Commented out some seeds for DB Seeding.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,18 +11,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
-        $this->call(DivisionsTableSeeder::class);
-        $this->call(LocationsTableSeeder::class);
-        $this->call(TicketsStatusesTableSeeder::class);
-        $this->call(TicketsTypesTableSeeder::class);
-        $this->call(TicketsPrioritiesTableSeeder::class);
-        $this->call(StatusesTableSeeder::class);
-        $this->call(WarrantyTypesTableSeeder::class);
-        $this->call(SuppliersTableSeeder::class);
-        $this->call(PcspecsTableSeeder::class);
-        $this->call(AssetTypesTableSeeder::class);
-        $this->call(ManufacturersTableSeeder::class);
-        $this->call(AssetModelsTableSeeder::class);
+      /**
+       * These DB Seeds will create your default data you will require.
+       */
+      $this->call(TicketsStatusesTableSeeder::class);
+      $this->call(TicketsTypesTableSeeder::class);
+      $this->call(TicketsPrioritiesTableSeeder::class);
+      $this->call(StatusesTableSeeder::class);
+      $this->call(WarrantyTypesTableSeeder::class);
+
+      /**
+       * The commented lines are those that can be used to generate data for testing.
+       * Uncomment those you'd like to seed before running 'artisan db:seed'.
+       */
+
+      // $this->call(UsersTableSeeder::class);
+      // $this->call(DivisionsTableSeeder::class);
+      // $this->call(LocationsTableSeeder::class);
+      // $this->call(SuppliersTableSeeder::class);
+      // $this->call(PcspecsTableSeeder::class);
+      // $this->call(AssetTypesTableSeeder::class);
+      // $this->call(ManufacturersTableSeeder::class);
+      // $this->call(AssetModelsTableSeeder::class);
     }
 }
