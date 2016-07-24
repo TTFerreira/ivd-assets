4d3e5cd7950af0170d395e99d04f3f721af041d9
Terry Ferreira <terry@pixelcandy.co.za>
2016-07-24 21:19:17 +0200

    Add database reset task
    Add database reset task

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+      $schedule->command('migrate:reset')->everyFiveMinutes();
+      $schedule->command('migrate')->everyFiveMinutes();
+      $schedule->command('db:seed')->everyFiveMinutes();
     }
 }
